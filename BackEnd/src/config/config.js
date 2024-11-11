658438e24b143c8b0ccc27b610b5fb95aa81c0b4
sabariswaran832 <sabariswaran832@gmail.com>
2024-11-11 21:45:47 +0530

    Update config.js

--- BackEnd/src/config/config.js
+++ BackEnd/src/config/config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Database connection
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/employeeDB", {
+    await mongoose.connect("process.env.MONGO_URI", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
