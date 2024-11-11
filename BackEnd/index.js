9626d57c92c3b0c7fad975a23026a4ed967bbb6e
sabariswaran832 <sabariswaran832@gmail.com>
2024-11-11 23:25:15 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -4,15 +4,20 @@ const db=require("./src/config/config")
 const app = express();
 const cors=require("cors")
 const bodyParser = require('body-parser');
-app.use(cors({
-    origin: 'http://localhost:3000', // Allow only requests from your React app
-    methods: ['GET', 'POST','PUT','DELETE'],        // Allow specific methods
-    credentials: true                // Allow credentials if needed
+
+// app.use(cors({
+//     origin: 'http://localhost:3000', // Allow only requests from your React app
+//     methods: ['GET', 'POST','PUT','DELETE'],        // Allow specific methods
+//     credentials: true                // Allow credentials if needed
     
-  }));
-  
-  app.use(express.json({limit: '50mb'}));
-  app.use(express.urlencoded({limit: '50mb'}));
+//   }));
+
+  app.use(cors('*'))
+
+
+  // app.use(bodyParser.json())
+  app.use(bodyParser.json({limit: '50mb'}));
+  app.use(bodyParser.urlencoded({limit: '50mb',extended:true}));
 db()
 // 
 app.use('/app', routes);
